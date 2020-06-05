a28bcb0d8542fb4bdbd6f1af7a73af541b507017
Arne De Veylder <arne.de.veyder@telenet.be>
2020-06-05 12:27:17 +0200

    Added blog template

--- Flask_app.py
+++ Flask_app.py
@@ -4,6 +4,16 @@ from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0fff506caeff08d660240ee10e953e9'
 
+blog_posts = [
+    {'title': 'Test Post 1',
+    'content': 'Dit is een erste test post',
+    'date': '05/06/2020'},
+    {'title': 'Test Post 2',
+    'content': 'Dit is een tweede test post',
+    'date': '05/06/2020'}
+]
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -27,7 +37,10 @@ def login():
     form = LoginForm()
     return render_template('login.html', form=form, title='Login')
 
-
+@app.route('/blog')
+def blog():
+    posts = blog_posts
+    return render_template('blog.html', title='Blog', blog_posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PersonalSite
 Persoonlijke site, flask leren en in toekomst hopelijk naar PWA
+
+## Afgewerkte functies
+Hieronder staan de functies opgegelijst die al geimplementeeerd zijn
+* Main Layout
+* Basic login en register form
+
+## Functies die nog komen
+Deze functies moeten op termijn afgewerkt zijn
+* Werkende login
+* Werkende register
+* Blog layout
+* Databank juist verbinden
+* Chat applicatie
+* Foto galerij
+* Blog en projects mergen
